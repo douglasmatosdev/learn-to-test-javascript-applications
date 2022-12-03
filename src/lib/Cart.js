e00a19968d1ffc373ad01203e6ea0337b3354f47
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-03 10:25:35 -0300

    update method add(), implement method remve() and checkout()

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,13 +1,32 @@
+import remove from 'lodash/remove';
+import find from 'lodash/find';
+
 export default class Cart {
   items = [];
 
   add(item) {
+    const itemToFind = { product: item.product };
+
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
     this.items.push(item);
+  }
+
+  remove(product) {
+    remove(this.items, { product });
   }
 
   getTotal() {
     return this.items.reduce((acc, item) => {
       return acc + item.quantity * item.product.price;
     }, 0);
+  }
+
+  checkout() {
+    return {
+      total: this.getTotal(),
+      items: this.items,
+    };
   }
 }
--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,26 +2,82 @@ import Cart from './Cart';
 
 describe('Cart', () => {
   let cart;
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388,
+  };
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 41872,
+  };
 
   beforeEach(() => {
     cart = new Cart();
   });
 
-  it('should  return 0 when getTotal() is executed in a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
+  describe('getTotal()', () => {
+    it('should  return 0 when getTotal() is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+
+    it('should multiply  quantity and price and receive the total amount', () => {
+      const item = {
+        product: {
+          title: 'Adidas running shoes - men',
+          price: 35388, // 352.88 | R$ 383,88
+        },
+        quantity: 2, //70776
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
+    });
+
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then remove', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+    });
   });
 
-  it('should multiply  quantity and price and receive the total amount', () => {
-    const item = {
-      product: {
-        title: 'Adidas running shoes - men',
-        price: 35388, // 352.88 | R$ 383,88
-      },
-      quantity: 2, //70776
-    };
+  describe('checkout()', () => {
+    it('should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
 
-    cart.add(item);
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
 
-    expect(cart.getTotal()).toEqual(70776);
+      expect(cart.checkout()).toMatchSnapshot();
+    });
   });
 });
