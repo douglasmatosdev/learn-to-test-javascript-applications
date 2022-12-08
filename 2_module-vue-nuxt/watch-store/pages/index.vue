57c9306b021e3653a3f7c65a9f9913cabd46711b
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-08 20:42:34 -0300

    installation and setup miragejs

--- 2_module-vue-nuxt/watch-store/pages/index.vue
+++ 2_module-vue-nuxt/watch-store/pages/index.vue
@@ -1,26 +1,39 @@
 <template>
   <main class="my-8">
-    <div class="container mx-auto px-6">
+    <div v-if="errorMessage === ''" class="container mx-auto px-6">
       <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
       <span class="mt-3 text-sm text-gray-500">200+ Products</span>
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
-        <product-card v-for="product in products" :key="product.id" />
+        <product-card
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
       </div>
     </div>
+    <h3 v-else class="text-center text-2x1">{{ errorMessage }}</h3>
   </main>
 </template>
 
 <script>
-import ProductCard from '@/components/ProductCard'
-import products from '@/mocks/products.json'
+import ProductCard from '@/components/ProductCard';
+
 export default {
   components: { ProductCard },
-  computed: {
-    products() {
-      return products
-    },
+  data() {
+    return {
+      products: [],
+      errorMessage: '',
+    };
   },
-}
+  async created() {
+    try {
+      this.products = (await this.$axios.get('/api/products')).data.products;
+    } catch (error) {
+      this.errorMessage = 'Problemas ao carregra a lista!';
+    }
+  },
+};
 </script>
