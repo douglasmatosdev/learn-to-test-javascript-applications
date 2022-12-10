846e721cda8345207f67b5162afe6de7de546da1
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-10 07:44:53 -0300

    should emit the event addToCart with product object when button gets clicked

---
+++ 2_module-vue-nuxt/watch-store/components/ProductCard.unit.spec.js
@@ -0,0 +1,55 @@
+import { mount } from '@vue/test-utils';
+import ProductCard from '@/components/ProductCard';
+import { makeServer } from '@/miragejs/server';
+
+let server;
+const mountProductCard = () => {
+  const product = server.create('product', {
+    title: 'Relógio bonito',
+    price: '23.00',
+    image:
+      'https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80',
+  });
+  return {
+    wrapper: mount(ProductCard, {
+      propsData: {
+        product,
+      },
+    }),
+    product,
+  };
+};
+
+describe('ProductCard - unit', () => {
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  beforeEach(() => {
+    server.shutdown();
+  });
+
+  it('should match snapshot', () => {
+    const { wrapper } = mountProductCard();
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should mount the component', () => {
+    const { wrapper } = mountProductCard();
+
+    expect(wrapper.vm).toBeDefined();
+    expect(wrapper.text()).toContain('Relógio bonito');
+    expect(wrapper.text()).toContain('23.00');
+  });
+
+  it('should emit the event addToCart with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard();
+
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted().addToCart).toBeTruthy();
+    expect(wrapper.emitted().addToCart.length).toBe(1);
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
+  });
+});
