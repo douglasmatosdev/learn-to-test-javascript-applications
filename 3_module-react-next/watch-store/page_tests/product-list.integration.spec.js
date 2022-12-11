4d3b949252ae171ff75e006e00128a33c11e5368
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-11 18:27:07 -0300

    TDD-integration tests part 7

--- 3_module-react-next/watch-store/page_tests/product-list.integration.spec.js
+++ 3_module-react-next/watch-store/page_tests/product-list.integration.spec.js
@@ -1,7 +1,8 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import ProductList from '../pages';
 import { makeServer } from '../miragejs/server';
 import { Response } from 'miragejs';
+import userEvent from '@testing-library/user-event';
 
 const renderProductList = () => {
   render(<ProductList />);
@@ -55,17 +56,27 @@ describe('ProductList', () => {
   });
 
   fit('should filter the product list when a search is performed', async () => {
+    const searchTerm = 'Relógio bonito';
     server.createList('product', 2);
 
     server.create('product', {
-      title: 'Relógio bonito',
+      title: searchTerm,
     });
 
     renderProductList();
 
     await waitFor(() => {
-      screen.getAllByTestId('product-card');
-      screen.debug();
+      expect(screen.getAllByTestId('product-card')).toHaveLength(3);
+    });
+
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, searchTerm);
+    await fireEvent.submit(form);
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(1);
     });
   });
 
