eb2bdf88bf99ecd739ff95634a7be634d758202a
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-11 18:54:46 -0300

    TDD-integration tests part 9

--- 3_module-react-next/watch-store/components/search.js
+++ 3_module-react-next/watch-store/components/search.js
@@ -9,6 +9,14 @@ export default function Search({ doSearch }) {
     doSearch(term);
   };
 
+  const inputHandler = (ev) => {
+    setTerm(ev.target.value);
+
+    if (ev.target.value === '') {
+      doSearch('');
+    }
+  };
+
   return (
     <form
       onSubmit={submitHandler}
@@ -30,7 +38,7 @@ export default function Search({ doSearch }) {
 
       <input
         value={term}
-        onChange={(ev) => setTerm(ev.target.value)}
+        onInput={inputHandler}
         className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
         type="search"
         placeholder="Search"
