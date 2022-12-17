95fc14048fa7924f30160001d0feedbb320b722c
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-17 08:23:27 -0300

    TDD - methods remove and removeAll

--- 3_module-react-next/watch-store/store/cart/index.js
+++ 3_module-react-next/watch-store/store/cart/index.js
@@ -26,9 +26,33 @@ export const useCartStore = create((set) => {
       },
       add(product) {
         setState(({ state }) => {
-          if (!state.products.includes(product)) {
+          const doesntExist = !state.products.find(({ id }) => id === product.id);
+
+          if (doesntExist) {
             state.products.push(product);
+            state.open = true;
           }
+        });
+      },
+      remove(product) {
+        setState(({ state }) => {
+          const exists = !!state.products.find(({ id }) => id === product.id);
+
+          if (exists) {
+            state.products = state.products.filter(({ id }) => {
+              return id !== product.id;
+            });
+          }
+        });
+      },
+      removeAll() {
+        setState(({ state }) => {
+          state.products = [];
+        });
+      },
+      reset() {
+        setState((store) => {
+          store.state = initialState;
         });
       },
     },
