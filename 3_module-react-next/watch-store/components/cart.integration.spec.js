38f9985fe0a1eec2c4a562176cc0ef9ce5c0207e
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-17 07:15:47 -0300

    cart integration tests

---
+++ 3_module-react-next/watch-store/components/cart.integration.spec.js
@@ -0,0 +1,73 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { screen, render } from '@testing-library/react';
+import { useCartStore } from '../store/cart';
+import { makeServer } from '../miragejs/server';
+import userEvent from '@testing-library/user-event';
+import Cart from './cart';
+
+describe('Cart', () => {
+  let server;
+  let result;
+  let spy;
+  let add;
+  let toggle;
+  let reset;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+    result = renderHook(() => useCartStore()).result;
+    add = result.current.actions.add;
+    toggle = result.current.actions.toggle;
+    reset = result.current.actions.reset;
+    spy = jest.spyOn(result.current.actions, 'toggle');
+  });
+
+  afterEach(() => {
+    server.shutdown();
+    jest.clearAllMocks();
+  });
+
+  it('should add css "hidden" in the component', () => {
+    render(<Cart />);
+
+    expect(screen.getByTestId('cart')).toHaveClass('hidden');
+  });
+
+  it('should add css "hidden" in the component', () => {
+    act(() => {
+      toggle();
+    });
+
+    render(<Cart />);
+
+    expect(screen.getByTestId('cart')).not.toHaveClass('hidden');
+  });
+
+  it('should call store toggle() twice', () => {
+    render(<Cart />);
+
+    const button = screen.getByTestId('close-button');
+
+    act(() => {
+      userEvent.click(button);
+      userEvent.click(button);
+      userEvent.click(button);
+    });
+
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+
+  it('should display 2 prodcts cards', () => {
+    const products = server.createList('product', 2);
+
+    act(() => {
+      for (const product of products) {
+        add(product);
+      }
+    });
+
+    render(<Cart />);
+
+    expect(screen.getAllByTestId('cart-item')).toHaveLength(2);
+  });
+});
