a6bf7163f30bbbc97c3639af91a26335c21a1869
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-17 08:48:32 -0300

    fixes

--- 3_module-react-next/watch-store/components/cart-item.unit.spec.js
+++ 3_module-react-next/watch-store/components/cart-item.unit.spec.js
@@ -39,7 +39,7 @@ describe('CartItem', () => {
   it('should increase quantity by 1 when second button is clicked', async () => {
     renderCartItem();
 
-    const [_, button] = screen.getAllByRole('button');
+    const button = screen.getByTestId('increase');
 
     await fireEvent.click(button);
 
@@ -49,7 +49,9 @@ describe('CartItem', () => {
   it('should decrease quantity by 1 when first button is clicked', async () => {
     renderCartItem();
 
-    const [buttonDecrease, buttonIncrease] = screen.getAllByRole('button');
+    const buttonIncrease = screen.getByTestId('increase');
+    const buttonDecrease = screen.getByTestId('decrease');
+
     const quantity = screen.getByTestId('quantity');
 
     await fireEvent.click(buttonIncrease);
@@ -62,7 +64,8 @@ describe('CartItem', () => {
   it('should not go below zero in the quantity', async () => {
     renderCartItem();
 
-    const [buttonDecrease] = screen.getAllByRole('button');
+    const buttonDecrease = screen.getByTestId('decrease');
+
     const quantity = screen.getByTestId('quantity');
 
     await fireEvent.click(buttonDecrease);
